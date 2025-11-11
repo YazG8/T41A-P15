621b6a5a4ce092798491c4d23b6fb51f26df3de2
Yaz Guerrero <yazmin.guerreroguev@gmail.com>
2025-11-11 14:08:16 -0600

    feature/182483

---
+++ 02_insert_data.sql
@@ -0,0 +1,14 @@
+INSERT INTO productos(nombre,precio,cantidad) values 
+('Teclado',180.00,5),
+('Mouse',110.00,6),
+('Mousepad',100.00,3),
+('Tarjeta',2000.00,2);
+
+INSERT INTO departamento(nombre) values
+('IT'),('RRHH'),('FINANZAS');
+
+INSERT INTO empleado(nombre,id_dep) values
+('yazmin Guerrero',1),
+('Rafael Sanchez',2),
+('David Vidales',2),
+('Cruz Angel',3);
