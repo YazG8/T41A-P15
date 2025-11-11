184d32ca6becc68720b36b51468497573d5dc3ce
Yaz Guerrero <yazmin.guerreroguev@gmail.com>
2025-11-11 14:16:16 -0600

    feature/182483

---
+++ test/test_function.py
@@ -0,0 +1,132 @@
+import psycopg2
+
+def test_descuento():
+  conn = psycopg2.connect(
+      dbname='test_db',
+      user='postgres',
+      password='postgres',
+      host='localhost',
+      port='5432'
+  )
+  conn.autocommit = False
+  try:
+      cur = conn.cursor()
+      query="SELECT calcular_desc(%s,%s)"
+      cur.execute(query,(50.0,1.5));
+      result=cur.fetchone()[0];
+      assert result is None;
+      cur.execute(query,(50.0,0.5));
+      result=cur.fetchone()[0];
+      assert result==25.0;
+      conn.commit()
+  except Exception as e:
+      raise e
+  finally:
+      #En caso de cualquier fallo en el try, el finally hará la limpieza.
+      conn.rollback()
+      cur.close()
+      conn.close()
+
+  def test_correo():
+    conn = psycopg2.connect(
+        dbname='test_db',
+        user='postgres',
+        password='postgres',
+        host='localhost',
+        port='5432'
+    )
+    conn.autocommit = False
+    try:
+        cur = conn.cursor()
+        query="SELECT validar_correo(%s)"
+        cur.execute(query,('cristiangodinez',));
+        result=cur.fetchone()[0];
+        assert result is False;
+        cur.execute(query,('cristianlimones3@gmail.com',));
+        result=cur.fetchone()[0];
+        assert result is True;
+        conn.commit()
+    except Exception as e:
+        raise e
+    finally:
+        #En caso de cualquier fallo en el try, el finally hará la limpieza.
+        conn.rollback()
+        cur.close()
+        conn.close()
+
+def test_stock():
+    conn = psycopg2.connect(
+        dbname='test_db',
+        user='postgres',
+        password='postgres',
+        host='localhost',
+        port='5432'
+    )
+    conn.autocommit = False
+    try:
+        cur = conn.cursor()
+        query="SELECT * from stock_menor(%s)"
+        cur.execute(query,(5,));
+        result=cur.fetchall();
+        nombres=[row[1] for row in result]
+        assert 'mousepad' in nombres
+        assert 'monitor' in nombres
+        conn.commit()
+    except Exception as e:
+        raise e
+    finally:
+        #En caso de cualquier fallo en el try, el finally hará la limpieza.
+        conn.rollback()
+        cur.close()
+        conn.close()
+
+def test_fecha():
+    conn = psycopg2.connect(
+        dbname='test_db',
+        user='postgres',
+        password='postgres',
+        host='localhost',
+        port='5432'
+    )
+    conn.autocommit = False
+    try:
+        cur = conn.cursor()
+        query="SELECT dia_semana(%s)"
+        cur.execute(query,('2025-11-08',));
+        result=cur.fetchone()[0];
+        assert result=='SATURDAY'
+        conn.commit()
+    except Exception as e:
+        raise e
+    finally:
+        #En caso de cualquier fallo en el try, el finally hará la limpieza.
+        conn.rollback()
+        cur.close()
+        conn.close()
+
+def test_empleados():
+    conn = psycopg2.connect(
+        dbname='test_db',
+        user='postgres',
+        password='postgres',
+        host='localhost',
+        port='5432'
+    )
+    conn.autocommit = False
+    try:
+        cur = conn.cursor()
+        query="SELECT total_depEmpleados(%s)"
+        cur.execute(query,(1,));
+        result=cur.fetchone()[0];
+        assert result==2
+        cur.execute(query,(2,));
+        result=cur.fetchone()[0];
+        assert result==1
+        conn.commit()
+    except Exception as e:
+        raise e
+    finally:
+        #En caso de cualquier fallo en el try, el finally hará la limpieza.
+        conn.rollback()
+        cur.close()
+        conn.close()
